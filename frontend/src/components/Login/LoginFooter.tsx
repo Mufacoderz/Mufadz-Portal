9d8614894fcfca28798be5572019f2116d751f6a
Muhammad <mf9555759@gmail.com>
2025-11-13 09:30:35 +0800

    login page

--- frontend/src/components/Login/LoginFooter.tsx
+++ frontend/src/components/Login/LoginFooter.tsx
@@ -1,9 +1,14 @@
-
+import { Link } from "react-router-dom"
 const LoginFooter = () => {
     return (
-        <div>
-
-        </div>
+        <>
+            <div className="mt-6 text-center text-xs sm:text-sm ">
+                Belum punya akun?{" "}
+                <Link to="/register" className="text-blue-700 dark:text-blue-300 hover:underline">
+                    Registrasi
+                </Link>
+            </div>
+        </>
     )
 }
 
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,12 +1,20 @@
-import ComingSoon from "../components/ComingSoon"
-const Login = () => {
-    return (
-        <>
-            <div className="py-10 dark:bg-gray-900 h-full">
-                <ComingSoon title="Login" />
-            </div>
-        </>
-    )
-}
 
-export default Login
+import MorphingShape2 from "../components/Blob2";
+import LoginForm from "../components/Login/LoginForm";
+import LoginFooter from "../components/Login/LoginFooter";
+
+export default function Register() {
+    return (
+        <div className="flex items-center justify-center min-h-screen dark:bg-gray-900">
+            <MorphingShape2/>
+            <div
+                className="sm:w-full max-w-md  bg-white/10 backdrop-blur-md border border-white/20 dark:border-gray-700/20 p-8 rounded-2xl shadow-2xl text-gray-700 dark:text-gray-300"
+            >
+
+                <LoginForm/>
+                <LoginFooter/>
+                
+            </div>
+        </div>
+    );
+}
